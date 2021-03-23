566be5fe308a1caef1868b849276b2e5df35c31b
THEosusi <bp20129shibaura-it.ac.j@shibaura-it.ac.jp>
2021-03-23 11:03:02 +0900

    auto commit

--- b_colofulcreatures.cpp
+++ b_colofulcreatures.cpp
@@ -1,3 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {}
+int main() {
+    long long N;
+    cin>>N;
+    vector<long long>vec(N);
+    for(long long i=0;i<N;i++){
+        cin>>vec.at(i);
+    }
+    sort(vec.begin(),vec.end());
+    long long sum=vec.at(0);
+    long long count=0;
+    bool aaa=true;
+    for(long long i=1;i<N;i++){
+        if(sum*2>=vec.at(i)){
+            sum+=vec.at(i);
+        }
+        else{
+            count++;
+            aaa=false;
+        }
+    }
+    if(aaa){
+        cout<<N<<endl;
+    }
+    else{
+        cout<<N-count<<endl;
+    }
+
+}
